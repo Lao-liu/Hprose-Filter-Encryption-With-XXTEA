de28733099be3ada52091c63a4e61407c343301d
Laoliu <Laoliu@lanmv.com>
2015-12-24 00:19:48 +0800

    重复序列化了

--- hprose-filter-init.php
+++ hprose-filter-init.php
@@ -3,32 +3,24 @@
 define ('XXTEA_KEY', ':LKJaskjdowkjlqwkejsdlifuPOiup247');
 
 function decrypt ($encrypt_string){
-	return hprose_unserialize(xxtea_decrypt(base64url_decode($encrypt_string), XXTEA_KEY));
+	return xxtea_decrypt(base64_decode($encrypt_string), XXTEA_KEY);
 }
 
 function encrypt ($content){
-	return base64url_encode(xxtea_encrypt(hprose_serialize($content), XXTEA_KEY));
-}
-
-function base64url_encode($data) {
-	return rtrim(strtr(base64_encode($data), '+/', '-_'), '='); 
-}
-
-function base64url_decode($data) {
-	return base64_decode(str_pad(strtr($data, '-_', '+/'), strlen($data) % 4, '=', STR_PAD_RIGHT));
+	return base64_encode(xxtea_encrypt($content, XXTEA_KEY));
 }
 
 class serverFilter implements \Hprose\Filter
 {
 	function inputFilter ( $data, $context )
     {
-    	file_put_contents('tmp.log', hprose_serialize($data) . PHP_EOL, FILE_APPEND);
+    	file_put_contents('tmp.log', $data . PHP_EOL, FILE_APPEND);
         return decrypt($data);
     }
 
     function outputFilter ( $data, $context )
     {
-    	file_put_contents('tmp.log', hprose_serialize($data) . PHP_EOL, FILE_APPEND);
+    	file_put_contents('tmp.log', $data . PHP_EOL, FILE_APPEND);
         return encrypt($data);
     }
 }
